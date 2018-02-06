59d79f0a2739a3c7145274c54ce2211fbc462482
786shankar <35800394+786shankar@users.noreply.github.com>
2018-02-06 16:57:58 +0530

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int n,a,d,l,m,i;
+    printf("enter the values of n,a,d");
+    scanf("%d\t%d\n%d",&n,&a,&d);
+    l=n*a;
+    m=l*d;
+    i=l+m;
+    printf("%d",i);
+}
