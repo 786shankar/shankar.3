be001c0b2f5aaa53931cd8be29a6bf1a3e1ea429
786shankar <35800394+786shankar@users.noreply.github.com>
2018-02-09 02:33:59 -0800

    Create mediam number 25.c

---
+++ mediam number 25.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    int a[100000],i,j,k,o;
+    printf("enter the size of array\n");
+    scanf("%d",&o);
+    printf("enter the values of array\n");
+    for(i=0;i<o;i++)
+    {
+        scanf("%d\t",&a[i]);
+    }
+    for(i=0;i<o;i++)
+    {
+        for(j=i+1;j<o;j++)
+        {
+            if(a[i]<a[j])
+            {
+                k=a[i];
+                a[i]=a[j];
+                a[j]=k;
+            }
+        }
+    }
+    printf("the maximum element is %d",a[i/2]);
+}
