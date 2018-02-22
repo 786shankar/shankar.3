02298bade37d7ba09e400b7438550746db4e3e80
786shankar <35800394+786shankar@users.noreply.github.com>
2018-02-22 02:17:32 -0800

    Create space counter33.c

---
+++ space counter33.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main() 
+{
+	char a[50];
+	int i,count=0;
+	printf("enter the sentence:");
+	scanf("%[^\n]s",a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==' ')
+		{
+			count=count+1;
+		}
+	}
+	printf("\nthe number of space in the line are:%d",count);
+}
